3f595ee63b38f502c6e280a3961317ef68b030d0
sebastianmuci <sebastianmuci@ufl.edu>
2022-01-29 03:48:35 -0500

    master push 1/29

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavigationBar.css";
+
+const NavigationBar = ( ) => {
+    return (
+      <header className="NavigationBar">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
+
+  export default NavigationBar;
